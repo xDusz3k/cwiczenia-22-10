cdf18c8545e3f22367accc71b806b1ebd0f3dcf4
michal.dyduch <michal.dyduch@BR-16>
2022-10-22 08:50:31 +0200

    Dodaj pliki projektów.

---
+++ cwiczenia 22-10/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace cwiczenia_22_10
+{
+    internal class Program
+    {
+
+        static void printTable(int[] tablica)
+        {
+            int[] table = { 3, 1, 2, 8, 5, 7 };
+            foreach (int i in table)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //int[] table = { 3, 6, -8, 9, 10, 12 };
+            //int max = table[0];
+            //foreach(int row in table)
+            //{
+            //    if (row > max)
+            //    {
+            //        max = row;
+            //    }
+            //}
+            //Console.WriteLine($"Maks: {max}");
+
+            //bool jest = true;
+            //int number = 3;
+            //for(int i = 2; i <= number ; i++)
+            //{
+            //    if(number % i == 0)
+            //    {
+            //        jest = false;
+            //    }
+
+            //}
+            //if (jest)
+            //{
+            //    Console.WriteLine("Jest pierwsza");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nie jest pierwsza");
+            //}
+
+            
+        }
+    }
+}
